584ccbec903b222d95e622c59c60ec4a7043cd12
Ondosh <tima.tomskihh@gmail.com>
2026-03-11 11:16:45 +0900

    deleted old repo

--- new_tasks/SimpleClassWithGUI/src/main/java/com/github/ondosh/simpleclasswithgui/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.github.ondosh.simpleclasswithgui;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- new_tasks/SimpleClassWithGUI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.github.ondosh.simpleclasswithgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.github.ondosh.simpleclasswithgui to javafx.fxml;
-    exports com.github.ondosh.simpleclasswithgui;
-}
