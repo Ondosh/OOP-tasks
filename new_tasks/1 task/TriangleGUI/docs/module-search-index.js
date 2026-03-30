444e679d8643ada78c7efdaebc3c2874b651217d
Ondosh <tima.tomskihh@gmail.com>
2026-03-30 14:03:30 +0900

    docs

---
+++ new_tasks/1 task/TriangleGUI/docs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.github.ondosh.trianglegui"}];updateSearchResults();
---
+++ new_tasks/1 task/TriangleGUI/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.ondosh.trianglegui","l":"MainApplication"},{"p":"com.github.ondosh.trianglegui","l":"MainController"}];updateSearchResults();
