fbd474cfab390c2730cc14ede23cddb512b925ea
Ondosh <tima.tomskihh@gmail.com>
2026-05-19 10:53:29 +0900

    coms

--- new_tasks/2 task/ChatBot/src/main/java/com/github/ondosh/chatbot/model/CurrentUser.java
+++ new_tasks/2 task/ChatBot/src/main/java/com/github/ondosh/chatbot/model/CurrentUser.java
@@ -12,8 +12,21 @@ public class CurrentUser {
     /** Профиль текущего пользователя (возраст, город). */
     private static UserProfile profile;
 
+    /**
+     * Ставит переданного пользователя как текущего пользователя сессии
+     * @param u
+     */
     public static void set(User u)               { user = u; }
+
+    /**
+     * Геттер User
+     * @return текущий пользователь сессии
+     */
     public static User get()                     { return user; }
 
+    /**
+     * Возвращает профиль текущего пользователя
+     * @return Профиль текущего пользователя
+     */
     public static UserProfile getProfile()       { return profile; }
 }
